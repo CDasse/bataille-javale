e5a15e33616b6215b2c777cf2cfee2c7bd238ce8
CDasse <clemence.dasse@yahoo.fr>
2026-03-09 16:02:10 +0100

    Fix: package and link issus

--- src/main/java/school/coda/jn_charlie_clemence/bataillejavale/Launcher.java
+++ src/main/java/school/coda/jn_charlie_clemence/bataillejavale/Launcher.java
@@ -1,6 +1,7 @@
-package school.coda.jn_charlie_clemence.bataillejavale.view;
+package school.coda.jn_charlie_clemence.bataillejavale;
 
 import javafx.application.Application;
+import school.coda.jn_charlie_clemence.bataillejavale.view.HelloApplication;
 
 public class Launcher {
     public static void main(String[] args) {
--- src/main/java/school/coda/jn_charlie_clemence/bataillejavale/view/HelloApplication.java
+++ src/main/java/school/coda/jn_charlie_clemence/bataillejavale/view/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("accueil.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/school/coda/jn_charlie_clemence/bataillejavale/accueil.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setTitle("Bataille Javal");
         stage.setScene(scene);
